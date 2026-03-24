bb867f08f4d3adda919d19a7f83b0aea6c7a43fe
G Thimmaraju <thimmaraju418@gmail.com>
2026-03-24 10:13:06 +0530

    added script s to access envt variable

--- tests/loginOrangehrm.spec.js
+++ tests/loginOrangehrm.spec.js
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
-import logindata from "../testdata/login.json"
+
 
 test('Login to Orange HRM', async ({ page }) => {
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    await page.locator('//input[@name="username"]').fill(logindata.username)
-    await page.locator('//input[@name="password"]').fill(logindata.password)
+    await page.locator('//input[@name="username"]').fill(process.env.APP_USERNAME)
+    await page.locator('//input[@name="password"]').fill(process.env.APP_PASSWORD)
     await page.locator('//button[@type="submit"]').click()
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
 
